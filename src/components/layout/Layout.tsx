132c6c9787fe24441f059e5f8540e5fbcf50548e
letthem <suyeon3662@naver.com>
2025-02-04 00:25:02 +0900

    Feat: gradient cursor 구현 (#4)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,11 @@
-import { Navbar } from '@/components/layout/Navbar';
 import { Outlet } from 'react-router-dom';
+import { Navbar } from '@/components/layout/Navbar';
+import { GradientCursor } from '@/components/ui/gradientCursor';
 
 export const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col bg-black">
+      <GradientCursor />
       <Navbar />
       <main className="mt-[88px]">
         <Outlet />
