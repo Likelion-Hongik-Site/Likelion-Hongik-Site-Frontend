36914fab9f1f28e47e6007cc494ebbbd020d7370
letthem <suyeon3662@naver.com>
2025-02-05 00:13:54 +0900

    Feat: HomePage phone ver UI 완성 (#6)

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,7 +5,7 @@ import { GradientCursor } from '@/components/ui/gradientCursor';
 export const Layout = () => {
   return (
     <div className="min-h-screen w-full flex flex-col items-center bg-black">
-      <div className="dt:min-w-screen flex flex-col ph:max-w-[375px] bg-black ph:px-2">
+      <div className="dt:min-w-screen flex flex-col dt:items-center ph:max-w-[440px] ph:items-start bg-black ph:px-2">
         <GradientCursor />
         <Navbar />
         <main className="mt-[88px]">
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -6,29 +6,31 @@ import likelion from '@/assets/webps/layout/likelion.webp';
 export const Navbar = () => {
   const nav = useNavigate();
   return (
-    <nav className="w-full bg-black h-[88px] fixed flex items-center justify-between z-999">
-      <div className="ml-11 flex items-center">
-        <div className="flex items-center mr-4 cursor-pointer" onClick={() => nav('/')}>
-          <img src={logo} alt="logo" className="w-[25px] h-[25px] mr-[10px]" />
-          <img src={likelion} alt="likelion logo" className="w-[120px] h-[19px]" />
+    <>
+      <nav className="ph:hidden dt:flex w-full bg-black h-[88px] fixed items-center justify-between z-999">
+        <div className="ml-11 flex items-center">
+          <div className="flex items-center mr-4 cursor-pointer" onClick={() => nav('/')}>
+            <img src={logo} alt="logo" className="w-[25px] h-[25px] mr-[10px]" />
+            <img src={likelion} alt="likelion logo" className="w-[120px] h-[19px]" />
+          </div>
+          <div className="flex gap-[14px]">
+            <Button href="https://www.instagram.com/likelion_hongik/">Instagram</Button>
+            <Button href="/contact" onClick={() => nav('/contact')}>
+              Contact
+            </Button>
+          </div>
         </div>
-        <div className="flex gap-[14px]">
-          <Button href="https://www.instagram.com/likelion_hongik/">Instagram</Button>
-          <Button href="/contact" onClick={() => nav('/contact')}>
-            Contact
-          </Button>
+        <div className="mr-10 flex items-center">
+          <div className="flex gap-[14px]">
+            <Button href="/archive" onClick={() => nav('/archive')}>
+              지난 활동
+            </Button>
+            <Button href="/recruit" onClick={() => nav('/recruit')}>
+              지원하기
+            </Button>
+          </div>
         </div>
-      </div>
-      <div className="mr-10 flex items-center">
-        <div className="flex gap-[14px]">
-          <Button href="/archive" onClick={() => nav('/archive')}>
-            지난 활동
-          </Button>
-          <Button href="/recruit" onClick={() => nav('/recruit')}>
-            지원하기
-          </Button>
-        </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
