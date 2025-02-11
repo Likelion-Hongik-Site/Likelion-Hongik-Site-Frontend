275b118804955e9c1e1274c5e29cbd45e6024127
letthem <suyeon3662@naver.com>
2025-02-11 20:51:39 +0900

    Feat: scrollbar 커스텀 (#13)

--- src/components/Archive/Review.tsx
+++ src/components/Archive/Review.tsx
@@ -1,4 +1,4 @@
-import rv1 from '@/assets/webps/review/1.webp';  
+import rv1 from '@/assets/webps/review/1.webp';
 import rv2 from '@/assets/webps/review/2.webp';
 import rv3 from '@/assets/webps/review/3.webp';
 import rv4 from '@/assets/webps/review/4.webp';
@@ -35,9 +35,13 @@ import rv36 from '@/assets/webps/review/36.webp';
 
 export const Review = () => {
   return (
-    <div className="w-[2160px] h-[575px] overflow-x-scroll flex relative items-center">
+    <div className="w-[2160px] h-[575px] overflow-x-scroll custom-scrollbar flex relative items-center">
       <div className="flex-none">
-        <img src={rv1} alt="review-1" className="h-[288px] object-cover ml-[20px] mr-[16px] rounded-[20px]" />
+        <img
+          src={rv1}
+          alt="review-1"
+          className="h-[288px] object-cover ml-[20px] mr-[16px] rounded-[20px]"
+        />
       </div>
       <div className="flex-none">
         <img src={rv2} alt="review-2" className="h-[288px] mr-[16px] object-cover rounded-[20px]" />
@@ -46,117 +50,213 @@ export const Review = () => {
         <img src={rv3} alt="review-3" className="h-[288px] mr-[16px] object-cover rounded-[20px]" />
       </div>
       <div className="flex gap-[26px] items-center">
-            <div className="flex-none">
-              <img
-                src={rv4}
-                alt="review-4"
-                className="h-[452px] object-cover transform rotate-[-4deg] relative z-0 rounded-[20px]"
-                style={{ marginLeft: '20px', boxShadow: '0px 0px 12px 2px #000' }}
-              />
-            </div>
-            <div className="flex-none">
-              <img
-                src={rv5}
-                alt="review-5"
-                className="h-[404px] object-cover transform rotate-[5deg] relative z-10 rounded-[20px]"
-                style={{ marginLeft: '-50px' , boxShadow: '0px 0px 12px 2px #000' }}
-              />
-            </div>
+        <div className="flex-none">
+          <img
+            src={rv4}
+            alt="review-4"
+            className="h-[452px] object-cover transform rotate-[-4deg] relative z-0 rounded-[20px]"
+            style={{ marginLeft: '20px', boxShadow: '0px 0px 12px 2px #000' }}
+          />
+        </div>
+        <div className="flex-none">
+          <img
+            src={rv5}
+            alt="review-5"
+            className="h-[404px] object-cover transform rotate-[5deg] relative z-10 rounded-[20px]"
+            style={{ marginLeft: '-50px', boxShadow: '0px 0px 12px 2px #000' }}
+          />
+        </div>
       </div>
       <div className="flex-none">
-        <img src={rv8} alt="review-8" className="h-[288px] object-cover ml-[500px] mr-[16px] rounded-[20px]" />
+        <img
+          src={rv8}
+          alt="review-8"
+          className="h-[288px] object-cover ml-[500px] mr-[16px] rounded-[20px]"
+        />
       </div>
       <div className="flex-none">
         <img src={rv9} alt="review-9" className="h-[288px] object-cover mr-[16px] rounded-[20px]" />
       </div>
       <div className="flex-none">
-        <img src={rv10} alt="review-10" className="h-[288px] object-cover mr-[16px] rounded-[20px]" />
+        <img
+          src={rv10}
+          alt="review-10"
+          className="h-[288px] object-cover mr-[16px] rounded-[20px]"
+        />
       </div>
       <div className="flex-none">
-        <img src={rv11} alt="review-11" className="h-[448px] object-cover transform rotate-[5deg] relative z-0 rounded-[20px]"
-        style={{ marginLeft: '-15px', boxShadow: '0px 0px 12px 2px #000' }} />
+        <img
+          src={rv11}
+          alt="review-11"
+          className="h-[448px] object-cover transform rotate-[5deg] relative z-0 rounded-[20px]"
+          style={{ marginLeft: '-15px', boxShadow: '0px 0px 12px 2px #000' }}
+        />
       </div>
       <div className="flex-none">
-        <img src={rv12} alt="review-12" className="h-[288px] object-cover mr-[14px] rounded-[20px]" />
+        <img
+          src={rv12}
+          alt="review-12"
+          className="h-[288px] object-cover mr-[14px] rounded-[20px]"
+        />
       </div>
       <div className="flex-none">
         <img src={rv13} alt="review-13" className="h-[288px] object-cover rounded-[20px]" />
       </div>
       <div className="flex-none">
-        <img src={rv14} alt="review-14" className="h-[392px] object-cover transform rotate-[-4deg] relative z-0 rounded-[20px]"
-          style={{ marginLeft: '-5px', boxShadow: '0px 0px 12px 2px #000' }} />
+        <img
+          src={rv14}
+          alt="review-14"
+          className="h-[392px] object-cover transform rotate-[-4deg] relative z-0 rounded-[20px]"
+          style={{ marginLeft: '-5px', boxShadow: '0px 0px 12px 2px #000' }}
+        />
       </div>
       <div className="flex-none">
-        <img src={rv15} alt="review-15" className="h-[288px] object-cover ml-[-16px] rounded-[20px]" />
+        <img
+          src={rv15}
+          alt="review-15"
+          className="h-[288px] object-cover ml-[-16px] rounded-[20px]"
+        />
       </div>
       <div className="flex-none">
         <img src={rv16} alt="review-16" className="h-[288px] object-cover rounded-[20px]" />
       </div>
       <div className="flex-none">
-        <img src={rv17} alt="review-17" className="h-[360px] object-cover transform rotate-[5deg] relative z-10 rounded-[20px]"
-          style={{ marginLeft: '-20px' , boxShadow: '0px 0px 12px 2px #000' }} />
+        <img
+          src={rv17}
+          alt="review-17"
+          className="h-[360px] object-cover transform rotate-[5deg] relative z-10 rounded-[20px]"
+          style={{ marginLeft: '-20px', boxShadow: '0px 0px 12px 2px #000' }}
+        />
       </div>
       <div className="flex-none">
-        <img src={rv18} alt="review-18" className="h-[288px] object-cover ml-[-32px] rounded-[20px]" />
+        <img
+          src={rv18}
+          alt="review-18"
+          className="h-[288px] object-cover ml-[-32px] rounded-[20px]"
+        />
       </div>
       <div className="flex-none">
-        <img src={rv19} alt="review-19" className="h-[288px] object-cover ml-[-16px] rounded-[20px]" />
+        <img
+          src={rv19}
+          alt="review-19"
+          className="h-[288px] object-cover ml-[-16px] rounded-[20px]"
+        />
       </div>
       <div className="flex-none">
-        <img src={rv20} alt="review-20" className="h-[412px] object-cover transform rotate-[-4deg] relative z-0 rounded-[20px]"
-          style={{ marginLeft: '-25px', boxShadow: '0px 0px 12px 2px #000' }} />
+        <img
+          src={rv20}
+          alt="review-20"
+          className="h-[412px] object-cover transform rotate-[-4deg] relative z-0 rounded-[20px]"
+          style={{ marginLeft: '-25px', boxShadow: '0px 0px 12px 2px #000' }}
+        />
       </div>
       <div className="flex-none">
-        <img src={rv21} alt="review-21" className="h-[288px] object-cover ml-[-32px] rounded-[20px]" />
+        <img
+          src={rv21}
+          alt="review-21"
+          className="h-[288px] object-cover ml-[-32px] rounded-[20px]"
+        />
       </div>
       <div className="flex-none">
-        <img src={rv22} alt="review-22" className="h-[288px] object-cover ml-[-16px] rounded-[20px]" />
+        <img
+          src={rv22}
+          alt="review-22"
+          className="h-[288px] object-cover ml-[-16px] rounded-[20px]"
+        />
       </div>
       <div className="flex-none">
-        <img src={rv23} alt="review-23" className="h-[356px] object-cover transform rotate-[-5deg] relative z-10 rounded-[20px]"
-          style={{ marginLeft: '-25px', boxShadow: '0px 0px 12px 2px #000' }} />
+        <img
+          src={rv23}
+          alt="review-23"
+          className="h-[356px] object-cover transform rotate-[-5deg] relative z-10 rounded-[20px]"
+          style={{ marginLeft: '-25px', boxShadow: '0px 0px 12px 2px #000' }}
+        />
       </div>
       <div className="flex-none">
-        <img src={rv24} alt="review-24" className="h-[408px] object-cover transform rotate-[5deg] relative z-0 rounded-[20px]"
-          style={{ marginLeft: '-15px' , boxShadow: '0px 0px 12px 2px #000' }} />
+        <img
+          src={rv24}
+          alt="review-24"
+          className="h-[408px] object-cover transform rotate-[5deg] relative z-0 rounded-[20px]"
+          style={{ marginLeft: '-15px', boxShadow: '0px 0px 12px 2px #000' }}
+        />
       </div>
       <div className="flex-none">
-        <img src={rv25} alt="review-25" className="h-[288px] object-cover ml-[-16px] rounded-[20px]" />
+        <img
+          src={rv25}
+          alt="review-25"
+          className="h-[288px] object-cover ml-[-16px] rounded-[20px]"
+        />
       </div>
       <div className="flex-none">
         <img src={rv26} alt="review-26" className="h-[288px] object-cover rounded-[20px]" />
       </div>
       <div className="flex-none">
-        <img src={rv27} alt="review-27" className="h-[384px]  object-cover transform rotate-[-4deg] relative z-10 rounded-[20px]"
-          style={{ marginLeft: '-15px', boxShadow: '0px 0px 12px 2px #000' }} />
+        <img
+          src={rv27}
+          alt="review-27"
+          className="h-[384px]  object-cover transform rotate-[-4deg] relative z-10 rounded-[20px]"
+          style={{ marginLeft: '-15px', boxShadow: '0px 0px 12px 2px #000' }}
+        />
       </div>
       <div className="flex-none">
-        <img src={rv28} alt="review-28" className="h-[288px] object-cover ml-[-16px] rounded-[20px]" />
+        <img
+          src={rv28}
+          alt="review-28"
+          className="h-[288px] object-cover ml-[-16px] rounded-[20px]"
+        />
       </div>
       <div className="flex-none">
         <img src={rv29} alt="review-29" className="h-[288px] object-cover rounded-[20px]" />
       </div>
       <div className="flex-none">
-        <img src={rv30} alt="review-30" className="h-[208px] object-cover transform rotate-[8.672deg] relative z-10 rounded-[20px]"
-          style={{ marginLeft: '-15px', marginBottom: '150px', boxShadow: '0px 0px 12px 2px #000' }} />
+        <img
+          src={rv30}
+          alt="review-30"
+          className="h-[208px] object-cover transform rotate-[8.672deg] relative z-10 rounded-[20px]"
+          style={{ marginLeft: '-15px', marginBottom: '150px', boxShadow: '0px 0px 12px 2px #000' }}
+        />
       </div>
       <div className="flex-none">
-        <img src={rv31} alt="review-31" className="h-[288px] object-cover ml-[-32px] rounded-[20px]" />
+        <img
+          src={rv31}
+          alt="review-31"
+          className="h-[288px] object-cover ml-[-32px] rounded-[20px]"
+        />
       </div>
       <div className="flex-none">
-        <img src={rv32} alt="review-32" className="h-[288px] object-cover ml-[-16px] rounded-[20px]" />
+        <img
+          src={rv32}
+          alt="review-32"
+          className="h-[288px] object-cover ml-[-16px] rounded-[20px]"
+        />
       </div>
       <div className="flex-none">
-        <img src={rv33} alt="review-33" className="h-[288px] object-cover mr-[16px] rounded-[20px]" />
+        <img
+          src={rv33}
+          alt="review-33"
+          className="h-[288px] object-cover mr-[16px] rounded-[20px]"
+        />
       </div>
       <div className="flex-none">
-        <img src={rv34} alt="review-34" className="h-[288px] object-cover mr-[16px] rounded-[20px]" />
+        <img
+          src={rv34}
+          alt="review-34"
+          className="h-[288px] object-cover mr-[16px] rounded-[20px]"
+        />
       </div>
       <div className="flex-none">
-        <img src={rv35} alt="review-35" className="h-[288px] object-cover mr-[16px] rounded-[20px]" />
+        <img
+          src={rv35}
+          alt="review-35"
+          className="h-[288px] object-cover mr-[16px] rounded-[20px]"
+        />
       </div>
       <div className="flex-none">
-        <img src={rv36} alt="review-36" className="h-[288px] object-cover mr-[76px] rounded-[20px]" />
+        <img
+          src={rv36}
+          alt="review-36"
+          className="h-[288px] object-cover mr-[76px] rounded-[20px]"
+        />
       </div>
     </div>
   );
